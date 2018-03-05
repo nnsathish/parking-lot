78a99c2e0496ccd8760fcd64de40e3ff605f2e79
Sathishkumar Natesan <nnsathish@gmail.com>
2018-03-06 04:39:26 +0530

    Update ParkingLot::Slot logic for specs to pass

--- lib/parking_lot/slot.rb
+++ lib/parking_lot/slot.rb
@@ -5,5 +5,23 @@ module ParkingLot
     def initialize(number)
       @number = number
     end
+
+    def park!(car)
+      return unless free?
+
+      self.car = car # TODO::Update Base#next_available_slot!!
+      'Allocated slot number: ' << self.number.to_s
+    end
+
+    def leave!
+      return if free?
+
+      self.car = nil
+      "Slot number #{self.number} is free"
+    end
+
+    def free?
+      car.nil?
+    end
   end
 end
